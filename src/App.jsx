f2e963ee485ef75fde9e06a062847daf2d7bbd6c
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-19 21:24:32 -0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React from "react";
 import Nav from "./Nav/nav.jsx";
 import Background from "./body/background";
-import Farms from "./body/farms/index.tsx";
-import Footer from './Footer'
-import Wb3 from 'web3'
-import getWeb3 from './utils/web3Utils';
-import util from './utils/aprLib/index.js';
-import nativeFarmAbi from './utils/nativeFarmAbi.js';
+import Farms from "./body/farms/index";
+import Footer from "./Footer";
+import Wb3 from "web3";
+import getWeb3 from "./utils/web3Utils";
+import util from "./utils/aprLib/index.js";
+import nativeFarmAbi from "./utils/nativeFarmAbi.js";
 
-export default class App extends React.Component{
+export default class App extends React.Component {
+  componentDidMount = async () => {
+    getWeb3()
+      .then(() => {
+        this.forceUpdate();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-  componentDidMount = async()=>{
-     getWeb3().then(()=>{
-      this.forceUpdate()
-     }).catch((e)=>{console.log(e)})
+  render() {
+    return (
+      <div className="App">
+        <main className="app preload">
+          <Nav />
+          <Background />
+          <Farms />
+          <Footer />
+        </main>
+      </div>
+    );
   }
-
-
-  render (){
-    return(<div className="App">
-      <main className="app preload">
-        <Nav />
-        <Background />
-        <Farms />
-        <Footer />
-      </main>
-    </div>)
-  }
-  
-  }
+}
