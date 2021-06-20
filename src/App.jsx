aa22137db8a301e6855e5a8c78b5c0af27feacd1
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-19 21:05:15 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Nav from "./Nav/nav.jsx";
 import Background from "./body/background";
 import Farms from "./body/farms/index.tsx";
-import Footer from './Footer/footer.tsx'
+import Footer from './Footer'
 import Wb3 from 'web3'
 import getWeb3 from './utils/web3Utils';
 import util from './utils/aprLib/index.js';
