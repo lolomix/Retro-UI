7ae70d5f457c4d54724a29c799454ac63c609076
francovilla <42851404+francovilla@users.noreply.github.com>
2021-06-22 07:16:15 -0700

    Update

--- src/body/farms/poolsd/pool.jsx
+++ src/body/farms/poolsd/pool.jsx
@@ -346,7 +346,7 @@ export default function Pool(props) {
         apr,
         userBalance: balanced
       });
-    } catch (error) {}
+    } catch (error) {console.log(error)}
   };
 
   async function calculateApr(pool, balance) {
