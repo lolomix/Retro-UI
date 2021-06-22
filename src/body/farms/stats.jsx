b2e4eed5b079e34b874e445e3022ae05e642245a
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-22 17:44:44 -0400

    update qbert pending

--- src/body/farms/stats.jsx
+++ src/body/farms/stats.jsx
@@ -1,19 +1,20 @@
-
 import { useState, useEffect } from "react";
 
 export default function Stats() {
-  let [data, setData] = useState({pending: 0, deposit: 0, loaded: false})
-  useEffect(()=>{
-    if(!data.loaded){
-      setData({loaded: true})
-      console.log('stats load')
+  let [data, setData] = useState({ pending: 0, deposit: 0, loaded: false });
+  useEffect(() => {
+    if (!data.loaded) {
+      setData({ loaded: true });
+      console.log("stats load");
       setInterval(() => {
-        setData({pending: window.ts.pending, deposited: window.ts.deposited, loaded: true})
-        
+        setData({
+          pending: window.ts.pending,
+          deposited: window.ts.deposited,
+          loaded: true
+        });
       }, 3000);
-      
-    }  
-  })
+    }
+  });
 
   function formatNumber(num) {
     if (num > 999 && num < 1000000) {
@@ -24,16 +25,21 @@ export default function Stats() {
       return num;
     }
   }
-  return(
+  return (
     <div className="stats-stripe">
-                <div className="btn show-hide"></div>
-                <div className="txt deposit-ttl">My total deposit:</div>
-                <div className={'txt total-deposit loading'}>{data.deposited? '$'+ formatNumber((data.deposited).toFixed(2)): '***'}</div>
-                <div className="txt qbert-ttl">QBert pending:</div>
-                <div className="txt qbert-pending loading">
-                    <span className="amount">{data.pending? (data.pending).toFixed(3): '***'}</span>
-                </div>
-                <div className="btn harvest-all disabled">Harvest All</div>
-            </div>
-  )
+      <div className="btn show-hide"></div>
+      <div className="txt deposit-ttl">My total deposit:</div>
+      <div className={"txt total-deposit loading"}>
+        {data.deposited ? "$" + formatNumber(data.deposited.toFixed(2)) : "***"}
+      </div>
+      <div className="txt qbert-ttl">QBert pending:</div>
+      <div
+        className="txt qbert-pending loading"
+        style={{ color: "var(--c-primary)" }}
+      >
+        {data.pending ? data.pending.toFixed(3) : "***"}
+      </div>
+      <div className="btn harvest-all disabled">Harvest All</div>
+    </div>
+  );
 }
