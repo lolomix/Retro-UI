b0fdcab7bdb2cda34166a704551a8415dbbbed46
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-23 01:37:43 -0400

    up buttons

--- src/Nav/nav.jsx
+++ src/Nav/nav.jsx
@@ -1,6 +1,6 @@
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logos/logo.png";
 import qbertpxl from "../assets/logos/qbertpxl.png";
-import React, { useEffect, useState } from "react";
 
 export default function Nav() {
   var [menu, setMenu] = useState(false);
