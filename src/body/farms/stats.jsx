a32d3d2331685369ac07a7ac7e9a22b3bab917e4
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-24 02:18:08 -0400

    update css styles

--- src/body/farms/stats.jsx
+++ src/body/farms/stats.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import poolAbi from "../../utils/nativeFarmAbi";
 const farmAddress = "0x470D6c58470E361a72934399603115d5CAb08aC0";
-import config from '../../pools_config.json'
+import config from "../../pools_config.json";
 export default function Stats() {
   let [data, setData] = useState({ pending: 0, deposit: 0, loaded: false });
   useEffect(() => {
@@ -30,30 +30,40 @@ export default function Stats() {
 
   async function harvestall() {
     let pool = new web3.eth.Contract(poolAbi, farmAddress);
-     for (let i = 0; i < 1000; i++) {
-       try {
-        let balance = await pool.methods.pendingNATIVE(config[i].id, window.account).call()
-        console.log(balance)
-        if(balance > 1e8){
-          pool.methods.withdraw(config[i].id, 0).send({from: window.account})
+    for (let i = 0; i < 1000; i++) {
+      try {
+        let balance = await pool.methods
+          .pendingNATIVE(config[i].id, window.account)
+          .call();
+        console.log(balance);
+        if (balance > 1e8) {
+          pool.methods.withdraw(config[i].id, 0).send({ from: window.account });
         }
-       } catch (error) { 
-       }
-      
-
+      } catch (error) {}
     }
-    console.log('finished')
+    console.log("finished");
   }
-  return(
+  return (
     <div className="stats-stripe">
-                <div className="btn show-hide"></div>
-                <div className="txt deposit-ttl">My total deposit:</div>
-                <div className={'txt total-deposit loading'}>{data.deposited? '$'+ formatNumber((data.deposited).toFixed(2)): '***'}</div>
-                <div className="txt qbert-ttl">QBert pending:</div>
-                <div className="txt qbert-pending loading">
-                    <span className="amount">{data.pending? (data.pending).toFixed(3): '***'}</span>
-                </div>
-                <div onClick={()=>{harvestall()}} className="btn harvest-all disabled">Harvest All</div>
-            </div>
-  )
+      <div className="btn show-hide"></div>
+      <div className="txt deposit-ttl">My total deposit:</div>
+      <div className={"txt total-deposit loading"}>
+        {data.deposited ? "$" + formatNumber(data.deposited.toFixed(2)) : "***"}
+      </div>
+      <div className="txt qbert-ttl">QBert pending:</div>
+      <div className="txt qbert-pending loading">
+        <span className="amount">
+          {data.pending ? data.pending.toFixed(3) : "***"}
+        </span>
+      </div>
+      <div
+        onClick={() => {
+          harvestall();
+        }}
+        className="btn outlined harvest-all"
+      >
+        Harvest All
+      </div>
+    </div>
+  );
 }
