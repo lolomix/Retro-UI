46b1cee6f6880aba331b3a25c832832c5575ae50
RetroDefi BSC <82356199+RetroDefi@users.noreply.github.com>
2021-06-27 02:15:07 -0400

    update

--- src/utils/getTvl.js
+++ src/utils/getTvl.js
@@ -1,92 +1,91 @@
-import config from '../pools_config.json'
-import Web3 from 'web3';
-const Contract = require('web3-eth-contract');
-const provider = 'https://data-seed-prebsc-1-s1.binance.org:8545'
+import config from "../pools_config.json";
+import Web3 from "web3";
+const Contract = require("web3-eth-contract");
+const provider = "https://data-seed-prebsc-1-s1.binance.org:8545";
 
-import farmAbi from './nativeFarmAbi';
-const poolAbi = require('./aprLib/pool')
-const farmAddress = "0x470D6c58470E361a72934399603115d5CAb08aC0";
-const axios = require('axios')
-
+import farmAbi from "./nativeFarmAbi";
+const poolAbi = require("./aprLib/pool");
+const farmAddress = "0x939B132fb1fA5dC4C03a2a3c1d6a0D0A08A8d94b";
+const axios = require("axios");
 
 function send(results, status) {
   return new Promise(async (resolve, reject) => {
-          for (var i = 0; i < config.length; i++) {
-              let poolConfig = config[i]
-              const pool =  new window.web3.eth.Contract(farmAbi, farmAddress);
-              let deposited = await pool.methods.stakedWantTokens(poolConfig.id, window.account).call();
-              let price  = await tokenPrice(poolConfig)
-              console.log('xd')
-
-          }
-          resolve();
-        });
+    for (var i = 0; i < config.length; i++) {
+      let poolConfig = config[i];
+      const pool = new window.web3.eth.Contract(farmAbi, farmAddress);
+      let deposited = await pool.methods
+        .stakedWantTokens(poolConfig.id, window.account)
+        .call();
+      let price = await tokenPrice(poolConfig);
+      console.log("xd");
+    }
+    resolve();
+  });
 }
-
 
 async function tokenPrice(poolConfig) {
   if (!poolConfig.isLp) {
-
     let tokenPrice = await getTokenPrice(
       poolConfig.price.lpaddress,
       poolConfig.decimals
     );
     tokenPrice = tokenPrice[poolConfig.price.reserve];
     return tokenPrice;
-  } else{
-
+  } else {
     let value = await getLpPrice(
-      poolConfig.price.lpaddress, 
-      poolConfig.decimals)
-    value = value[poolConfig.price.reserve] * 2 
+      poolConfig.price.lpaddress,
+      poolConfig.decimals
+    );
+    value = value[poolConfig.price.reserve] * 2;
     let tokenPrice = await getTokenPrice(
       poolConfig.price.bnnlpaddress,
       poolConfig.decimals
     );
 
-    tokenPrice = tokenPrice[poolConfig.price.reserve]
-    return value * tokenPrice
-
-
+    tokenPrice = tokenPrice[poolConfig.price.reserve];
+    return value * tokenPrice;
   }
 }
 
-
 // Prices steels
-async function getTokenPrice(poolAddress, decimals){
-  
+async function getTokenPrice(poolAddress, decimals) {
   var pool = new Contract(poolAbi.data, poolAddress);
 
   let tokenInfo = await getTokensInfo(pool);
-  let bnbPrice = await axios.get('https://api.coingecko.com/api/v3/coins/binancecoin')
-  
-  let tokenprice0 = (tokenInfo._reserve1 / 10 ** 18) / (tokenInfo._reserve0 / 10 ** 8) * bnbPrice.data.market_data.current_price.usd;
-  let tokenprice1 = (tokenInfo._reserve0 / 10 ** 8) / (tokenInfo._reserve1 / 10 ** 18) * bnbPrice.data.market_data.current_price.usd;
-  console.log([tokenprice0 , tokenprice1 ])
-  return [tokenprice0 , tokenprice1 ]
+  let bnbPrice = await axios.get(
+    "https://api.coingecko.com/api/v3/coins/binancecoin"
+  );
+
+  let tokenprice0 =
+    (tokenInfo._reserve1 / 10 ** 18 / (tokenInfo._reserve0 / 10 ** 8)) *
+    bnbPrice.data.market_data.current_price.usd;
+  let tokenprice1 =
+    (tokenInfo._reserve0 / 10 ** 8 / (tokenInfo._reserve1 / 10 ** 18)) *
+    bnbPrice.data.market_data.current_price.usd;
+  console.log([tokenprice0, tokenprice1]);
+  return [tokenprice0, tokenprice1];
 }
 
-
 async function getTokensInfo(pool) {
-  
- 
-  let token0 = await pool.methods.token0().call()
-  let token1 = await pool.methods.token1().call()
-  let {_reserve0, _reserve1, _blockTimestampLast} = await pool.methods.getReserves().call()
-  return {_reserve0, _reserve1, _blockTimestampLast}
+  let token0 = await pool.methods.token0().call();
+  let token1 = await pool.methods.token1().call();
+  let {
+    _reserve0,
+    _reserve1,
+    _blockTimestampLast
+  } = await pool.methods.getReserves().call();
+  return { _reserve0, _reserve1, _blockTimestampLast };
 }
 
 async function getLpPrice(poolAddress, decimals) {
   let pool = Contract(poolAbi.data, poolAddress);
 
   let lpSupply = await pool.methods.totalSupply().call();
-  let {_reserve0, _reserve1, _blockTimestampLast} = await getTokensInfo()
-  let amount0 = (_reserve0 / 10 ** decimals) / (lpSupply / 10 ** 18)
-  let amount1 = (_reserve1 / 10 ** decimals) /(lpSupply / 10 ** 18) 
- 
-  return [amount0, amount1];
+  let { _reserve0, _reserve1, _blockTimestampLast } = await getTokensInfo();
+  let amount0 = _reserve0 / 10 ** decimals / (lpSupply / 10 ** 18);
+  let amount1 = _reserve1 / 10 ** decimals / (lpSupply / 10 ** 18);
 
-  
+  return [amount0, amount1];
 }
 
 export default send;
